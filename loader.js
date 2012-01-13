b1de796601566d122d182f05f4fa97e2c4820116
Ori Livneh <ori.livneh@gmail.com>
2012-01-13 09:34:58 -0500

    simple ajax loader

---
+++ loader.js
@@ -0,0 +1,30 @@
+/*jslint browser: true */
+"use strict";
+var ansi = ansi || {};
+
+ansi.load = function (url, charset, callback) {
+    var req = new window.XMLHttpRequest();
+
+    if (typeof callback === 'undefined') {
+        callback = charset;
+        charset = 'ibm437';
+    }
+    req.overrideMimeType('text/plain; charset=' + charset);
+    req.onreadystatechange = function () {
+        var LOADED = 4,
+            OK = 200;
+        if (req.readyState === LOADED) {
+            if (req.status === OK) {
+                callback(req.responseText);
+            } else {
+                throw new Error('load(): request failed.');
+            }
+        }
+    };
+    req.open('GET', url, true);
+    req.send(null);
+};
+
+ansi.load('/VIK.ANS', function (data) {
+    console.log(data);
+});
