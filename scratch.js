7768b30096dd0e128e6e7944252694ad4ab0b214
Ori Livneh <ori.livneh@gmail.com>
2012-01-13 10:18:10 -0500

    better folder layout

--- scratch.js
+++ scratch.js
@@ -2,7 +2,7 @@
 /*global ansi */
 "use strict";
 
-window.onload = ansi.load('VIK.ANS', function (data) {
+window.onload = ansi.load('static/VIK.ANS', function (data) {
     console.log("hello!");
     console.log(data.length);
     ansi.acid.init(data);
