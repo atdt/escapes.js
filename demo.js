9eb4b8e01d968d616aed9d237271dc9ef7605783
Ori Livneh <ori.livneh@gmail.com>
2012-02-15 16:00:42 -0500

    js tweaks

--- demo.js
+++ demo.js
@@ -3,14 +3,16 @@
 $.shuffle = function (array) {
     var tmp, current, top = array.length;
 
-    if(top) while(--top) {
-        current = Math.floor(Math.random() * (top + 1));
-        tmp = array[current];
-        array[current] = array[top];
-        array[top] = tmp;
+    if (top) {
+        while(--top) {
+            current = Math.floor(Math.random() * (top + 1));
+            tmp = array[current];
+            array[current] = array[top];
+            array[top] = tmp;
+        }
     }
     return array;
-}
+};
 
 // Trigger a custom event ('bottomed') on document
 // when user has scrolled to the bottom.
@@ -35,6 +37,7 @@ $(function () {
 
 $(function () {
     var job = $.Deferred().resolve(),
+        demo_ansi = $.ansiRender('/ansis/shnya-01.ans'),
         loading = new CanvasLoader('loader');
         files = $.shuffle([
             'agt77-01',  'ans-50a',   'ans-50c',   'ansi0796',  'ar-prlx2',
@@ -42,8 +45,16 @@ $(function () {
             'corin-04',  'corin-07',  'gs-acid',   'gs-shad1',  'km-fifty',
             'ks-ic1',    'ky-logo3',  'logo0495',  'may-acid',  'multi-03',
             'multi-05',  'multi-08',  'multi-10',  'ns-east',   'scrup-0b',
-            'sgi-03',    'shnya-01',  'tt-soh',    'txt-rz'
+            'sgi-03',    'tt-soh',    'txt-rz'
         ]);
+
+    function loadAnother() {
+        if (job.state() !== 'pending' && files.length !== 0) {
+            loading.show();
+            job = $.ansiRender('ansis/' + files.pop() + '.ans');
+            job.done(appendLoaded);
+        }
+    }
 
     function appendLoaded() {
         loading.hide();
@@ -52,11 +63,9 @@ $(function () {
 
     loading.setColor('#ffffff');
 
-    $(document).on('bottomed', function () {
-        if (job.state() !== 'pending' && files.length !== 0) {
-            loading.show();
-            job = $.ansiRender('ansis/' + files.pop() + '.ans');
-            job.done(appendLoaded);
-        }
+    $('.tryit').one('click', function () {
+        demo_ansi.done(appendLoaded);
+        $(document).on('bottomed', loadAnother);
     });
+
 });
